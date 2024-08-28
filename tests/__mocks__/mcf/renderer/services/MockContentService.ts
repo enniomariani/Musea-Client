7653c3d0a632170ae56d71fc9ad68d84df2d9b04
Ennio Mariani <emariani@gmx.ch>
2024-08-28 10:22:46 +0200

    - mock fixed

--- tests/__mocks__/mcf/renderer/services/MockContentService.ts
+++ tests/__mocks__/mcf/renderer/services/MockContentService.ts
@@ -1,23 +1,29 @@
 import {MockMediaStationRepository} from "../dataStructure/MockMediaStationRepository";
 import {ContentService} from "../../../../../src/js/mcf/renderer/services/ContentService";
 import {MockContentNetworkService} from "./MockContentNetworkService";
+import {MockMediaService} from "./MockMediaService";
 
 const mockMediaStationRepo:MockMediaStationRepository = new MockMediaStationRepository();
 const mockContentNetworkService:MockContentNetworkService = new MockContentNetworkService();
+const mockMediaService:MockMediaService = new MockMediaService();
 
 
 export class MockContentService extends ContentService {
     createContent: jest.Mock;
     changeName: jest.Mock;
+    deleteContent: jest.Mock;
+
     sendCommandPlay: jest.Mock;
     sendCommandStop: jest.Mock;
     sendCommandPause: jest.Mock;
     sendCommandSeek: jest.Mock;
 
     constructor() {
-        super(mockMediaStationRepo, mockContentNetworkService);
+        super(mockMediaStationRepo, mockContentNetworkService, mockMediaService);
         this.createContent = jest.fn();
         this.changeName = jest.fn();
+        this.deleteContent = jest.fn();
+
         this.sendCommandPlay = jest.fn();
         this.sendCommandStop = jest.fn();
         this.sendCommandPause = jest.fn();
