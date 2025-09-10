fd9f112c071f4f602253c5e1ffd92b699cc8ffc5
Ennio Mariani <mail@enniomariani.ch>
2025-09-10 17:22:39 +0200

    Fix super-params-call

--- tests/__mocks__/mcf/renderer/services/mediastation/MockMediaStationSyncService.ts
+++ tests/__mocks__/mcf/renderer/services/mediastation/MockMediaStationSyncService.ts
@@ -1,15 +1,19 @@
 import {MockNetworkService} from "tests/__mocks__/mcf/renderer/network/MockNetworkService";
 import {MockMediaStationRepository} from "tests/__mocks__/mcf/renderer/dataStructure/MockMediaStationRepository";
 import {MediaStationSyncService} from "src/mcf/renderer/services/mediastation/MediaStationSyncService";
+import {MockMediaAppConnectionService} from "tests/__mocks__/mcf/renderer/network/MockMediaAppConnectionService";
+import {MockMediaAppSyncService} from "tests/__mocks__/mcf/renderer/network/MockMediaAppSyncService";
 
 const mockNetworkService:MockNetworkService = new MockNetworkService();
 const mockMediaStationRepo:MockMediaStationRepository = new MockMediaStationRepository();
+const mockMediaAppConnectionService:MockMediaAppConnectionService = new MockMediaAppConnectionService();
+const mockMediaAppSyncService:MockMediaAppSyncService = new MockMediaAppSyncService();
 
 export class MockMediaStationSyncService extends MediaStationSyncService{
     sync: jest.Mock;
 
     constructor() {
-        super(mockNetworkService, mockMediaStationRepo);
+        super(mockNetworkService, mockMediaStationRepo, mockMediaAppConnectionService, mockMediaAppSyncService);
         this.sync = jest.fn();
     }
 }
